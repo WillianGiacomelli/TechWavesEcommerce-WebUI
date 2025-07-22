a49becd832338f4eefec62cac164623beb066f8a
Willian Giacomelli <willian2406@live.com>
2025-07-21 22:02:13 -0300

    feat: Implement menu category fetching in HeaderComponent with error handling

--- src/app/modules/public/components/header/component/header.component.ts
+++ src/app/modules/public/components/header/component/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MenuCategorieService } from '../../../services/http/menu-categorie.service';
 
 @Component({
   selector: 'app-public-header',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent  implements OnInit{
+  public menuCategories: any[] = [];
+  private _menuCategoryService = inject(MenuCategorieService);
+
+  ngOnInit(): void {
+    this._getMenuCategories();
+  }
+
+  private _getMenuCategories(): void {
+    this._menuCategoryService.getMenuCategories().subscribe({
+      next: (categories) => {
+        this.menuCategories = categories;
+        console.log('Menu Categories:', this.menuCategories);
+      },
+      error: (error) => {
+        console.error('Error fetching menu categories:', error);
+      }
+    });
+  }
 
 }
